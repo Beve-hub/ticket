31c441a1d6fc1a12287f6ba2423ac5b3f619ca7b
Believe <117173080+Beve-hub@users.noreply.github.com>
2024-11-19 09:31:55 +0100

    her

---
+++ client/src/pages/landingPage/Pricing.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const Pricing: React.FC<Props> = () => {
+    return (
+        <div>
+            price
+        </div>
+    )
+}
+
+export default Pricing
