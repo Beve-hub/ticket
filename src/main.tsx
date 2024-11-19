876f23daa42a19eb4abe6ee2a3cc04a54c094380
Believe <117173080+Beve-hub@users.noreply.github.com>
2024-11-19 09:13:11 +0100

    sew

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,4 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import '@mantine/notifications/styles.css';
-import './global.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
