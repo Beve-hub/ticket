4f3f26ab12c1d6c591e29e14d3f96b6f22e3caca
Believe <117173080+Beve-hub@users.noreply.github.com>
2024-11-30 20:41:42 +0100

    update

---
+++ client/src/pages/dashboard/eventdash/BookingCard.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const BookingCard: React.FC<Props> = () => {
+    return (
+        <div>
+            hello
+        </div>
+    )
+}
+
+export default BookingCard
---
+++ client/src/pages/dashboard/eventdash/TicketAna.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const TicketAna: React.FC<Props> = () => {
+    return (
+        <div>
+            ticket
+        </div>
+    )
+}
+
+export default TicketAna
